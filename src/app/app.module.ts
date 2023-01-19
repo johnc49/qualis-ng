b6cec4c5fcaae436fd568fcc6cbf7406b9cb52d3
John Chisowa <114460505+johnc49@users.noreply.github.com>
2023-01-19 16:06:02 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/pages/home/home.component';
+import { PaymentComponent } from './components/pages/payment/payment.component';
+import { AccountComponent } from './components/pages/account/account.component';
+import { ReportsComponent } from './components/pages/reports/reports.component';
+import { SettingComponent } from './components/pages/setting/setting.component';
+import { SearchbarComponent } from './components/shared/searchbar/searchbar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PaymentComponent,
+    AccountComponent,
+    ReportsComponent,
+    SettingComponent,
+    SearchbarComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
