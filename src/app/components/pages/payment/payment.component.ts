8903107053ed5f6d7b28e0b5b86f9343bca8dec7
John Chisowa <114460505+johnc49@users.noreply.github.com>
2023-01-23 18:15:39 +0200

    added payments

--- src/app/components/pages/payment/payment.component.ts
+++ src/app/components/pages/payment/payment.component.ts
@@ -1,25 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css'],
 })
-export class PaymentComponent implements OnInit {
-  selectedTab: string;
-
-  constructor() {
-    this.selectedTab = localStorage.getItem('selectedTab') || 'tab2';
-  }
-  ngOnInit() {
-    if (localStorage.getItem('selectedTab')) {
-      this.selectedTab = localStorage.getItem('selectedTab') || 'tab2';
-    }
-  }
-
-  setSelectedTab(tabName: string) {
-    this.selectedTab = tabName;
-    console.log(this.selectedTab);
-    localStorage.setItem('selectedTab', tabName);
-  }
-}
+export class PaymentComponent {}
